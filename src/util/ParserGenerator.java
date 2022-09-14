fea37bf65ba18f9f5d35dac8f4844c02df118238
Ali Afroozeh <afroozeh@gmail.com>
2022-09-14 13:39:37 +0200

    Working on Parse tree generation

--- src/util/ParserGenerator.java
+++ src/util/ParserGenerator.java
@@ -6,7 +6,11 @@ import io.usethesource.vallang.IValue;
 import io.usethesource.vallang.type.Type;
 import io.usethesource.vallang.type.TypeFactory;
 import org.iguana.grammar.Grammar;
+import org.iguana.iggy.gen.IggyParser;
+import org.iguana.parser.IguanaParser;
+import org.iguana.parser.ParseErrorException;
 import org.iguana.util.serialization.JsonSerializer;
+import org.iguana.utils.input.Input;
 import org.rascalmpl.values.IRascalValueFactory;
 import org.rascalmpl.values.RascalValueFactory;
 
@@ -25,11 +29,19 @@ public class ParserGenerator {
         RascalGrammarToIguanaGrammarConverter converter = new RascalGrammarToIguanaGrammarConverter();
         Grammar iguanaGrammar = converter.convert((IConstructor) grammar);
         System.out.println(iguanaGrammar);
-//        System.out.println(JsonSerializer.toJSON(iguanaGrammar));
+        IguanaParser parser = new IguanaParser(iguanaGrammar);
 
         return vf.function(ftype, (args, kwArgs) -> {
             // input is a string for now 
-            IString input = (IString) args[0];
+            IString inputString = (IString) args[0];
+            Input input = Input.fromString(inputString.getValue());
+            try {
+                parser.parse(input);
+            } catch (ParseErrorException e) {
+                throw e;
+            }
+
+            System.out.println(parser.getParseTree());
 
             // use the parser here to turn the string into a tree
             return vf.character('c')/*parse tree*/;
